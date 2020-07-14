bf3a5256d9f45180fac8e0f01292fa70cff9daf9
Martin Perussini <mperussini@frsf.utn.edu.ar>
2020-07-14 15:34:22 -0300

    Agregue dependencias en el gestor

--- src/main/java/DB/GestorDB.java
+++ src/main/java/DB/GestorDB.java
@@ -1,8 +1,13 @@
 package DB;
 
 import java.util.ArrayList;
+import com.complexible.stardog.*;
+import com.stardog.*;
 
-
+import java.io.FileInputStream;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GestorDB {
 
@@ -14,6 +19,7 @@ public class GestorDB {
 
     public boolean consultarDB(String promedio, String materias){
         //No se que devuelve todavia
+
         //TODO hacer la consulta
         String consulta="";
         return true;
