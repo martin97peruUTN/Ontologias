da28a699e9985dbe0053669820366bd498d96502
Martin Perussini <mperussini@frsf.utn.edu.ar>
2020-07-22 16:49:13 -0300

    Puse la consulta, solo con el año por ahora

--- src/main/java/DB/GestorDB.java
+++ src/main/java/DB/GestorDB.java
@@ -17,11 +17,20 @@ public class GestorDB {
         return true;
     }
 
-    public boolean consultarDB(String promedio, String materias){
+    public boolean consultarDB(String promedio, String materias, String anio){
         //No se que devuelve todavia
 
         //TODO hacer la consulta
-        String consulta="";
+        String consulta="SELECT DISTINCT ?x ?lib ?med\n" +
+                "WHERE{\n" +
+                "  ?x a :PostulanteABecaAdmisible.\n" +
+                "  ?x :numeroLegajo ?lib.\n" +
+                "  ?x :medicionFinal ?med.\n" +
+                "  ?x :seInscribeAConvocatoria ?conv.\n" +
+                "  ?conv :anioConvocatoria ?anioConv.\n" +
+                "  FILTER (?anioConv = "+anio+").\n" +
+                "}\n" +
+                "ORDER BY DESC (?med)";
         return true;
     }
 
